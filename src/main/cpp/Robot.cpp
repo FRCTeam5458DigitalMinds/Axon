aa5f3837b958d8a1276cffef2add781228ef2901
Reed Haffner <reedhaffner@pm.me>
2019-02-01 21:08:45 -0800

    THEORETICAL Move Turning
    not the way we want it but it should work

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -30,89 +30,54 @@ frc::Joystick JoyAccel1{0}, RaceWheel{2};
 
 // Gyro
 frc::ADXRS450_Gyro Gyro{}; 
-float lastSumAngle;
 
-void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-  
-  Gyro.Reset();
-
-  // Right Side Motors
-  /*BackRightBack.Set(ControlMode::PercentOutput, .2);
-  BackRightmid.Set(ControlMode::PercentOutput, .2);
-  BackRightFront.Set(ControlMode::PercentOutput, .2);
-  // Left Side Motors
-  BackLeftBack.Set(ControlMode::PercentOutput, -.1);
-  BackLeftmid.Set(ControlMode::PercentOutput, -.1);
-  BackLeftFront.Set(ControlMode::PercentOutput, -.1);
-  */
-}
-
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {}
-
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
-
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
-
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
-
-void Robot::TeleopInit() {}
-
+/* -+- Drive Functions -+- */
 void RightSpeedPercentage(float percentage){
   BackRightBack.Set(ControlMode::PercentOutput, percentage);
   BackRightmid.Set(ControlMode::PercentOutput, percentage);
   BackRightFront.Set(ControlMode::PercentOutput, percentage);
 }
-
 void LeftSpeedPercentage(float percentage){
   BackLeftBack.Set(ControlMode::PercentOutput, percentage);
   BackLeftmid.Set(ControlMode::PercentOutput, percentage);
   BackLeftFront.Set(ControlMode::PercentOutput, percentage);
 }
 
+/*Called on robot connection*/
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  
+  Gyro.Reset();
+}
+
+/*Called on every robot packet, no matter what mode*/
+void Robot::RobotPeriodic() {}
+
+/*Called when Autonomous is enabled*/
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+  if (m_autoSelected == kAutoNameCustom) {
+  } else {
+  }
+}
+
+/*Called every robot packet in auto*/
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+  } else {
+  }
+}
+
+/*Called when teleop is enabled*/
+void Robot::TeleopInit() {}
+
+/*Called every robot packet in teleop*/
 void Robot::TeleopPeriodic() {
   double yInput = JoyAccel1.GetY();
   double xInput = RaceWheel.GetX();
-
-  float sumAngle = Gyro.GetAngle();
-  float derivAngle = sumAngle - lastSumAngle;
-  float correctionAngle = (sumAngle * 0.1) + (derivAngle * .2);
 
   float turnFact = 0.9;
 
@@ -123,22 +88,25 @@ void Robot::TeleopPeriodic() {
   }
   // Drive Forward and Turn
   else if((yInput > 0.06 || yInput < -0.06) && (xInput > 0.01 || xInput < -0.01)){
-    RightSpeedPercentage(yInput + turnFact*(xInput));
-    std::cout << (-xInput) + 0.5*yInput << std::endl;
+    // If turning right
     if (xInput > 0.01){
-      RightSpeedPercentage(yInput + turnFact*(xInput));
-      LeftSpeedPercentage(yInput + turnFact*(xInput));
+      // Drive left at yInput percentage (100% Joystick 0% Wheel)
+      LeftSpeedPercentage(yInput);
+      // Drive right at yInput - half of xInput (100% Joystick, 50% Wheel) (At 100% on both Joystick and Wheel, the right wheels will turn at half the speed of the left wheels)
+      RightSpeedPercentage(yInput - (0.5*xInput));
     }
-    if (xInput < 0.01){
-      RightSpeedPercentage(-yInput + turnFact*(xInput));
-      LeftSpeedPercentage(yInput - turnFact*(xInput));              
+    // If turning left
+    else if (xInput < 0.01){
+      /// Drive right at yInput percentage (100% Joystick 0% Wheel)
+      RightSpeedPercentage(yInput);
+      // Drive left at yInput - half of xInput (100% Joystick, 50% Wheel) (At 100% on both Joystick and Wheel, the right wheels will turn at half the speed of the left wheels)
+      LeftSpeedPercentage(yInput - (0.5*xInput));       
     }
-    LeftSpeedPercentage(yInput + turnFact*(xInput));
   }
   // Drive Forward and no Turn
   else if(yInput > 0.06 || yInput < -0.06){
-    RightSpeedPercentage(-yInput - correctionAngle);
-    LeftSpeedPercentage(yInput - correctionAngle); 
+    RightSpeedPercentage(-yInput);
+    LeftSpeedPercentage(yInput); 
   }
   // Turn off motors if button (5) not pressed 
   else 
@@ -146,13 +114,12 @@ void Robot::TeleopPeriodic() {
     RightSpeedPercentage(0);
     LeftSpeedPercentage(0);
   }
-
-  lastSumAngle = sumAngle;  
-
 }
 
+/*Called every robot packet in testing mode*/
 void Robot::TestPeriodic() {}
 
+/*Starts the bot*/
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
 #endif
