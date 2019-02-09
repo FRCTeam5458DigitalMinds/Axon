1907088ff160d03fbc403624a9a2a487deef7122
Maximo Cazares <Maximo43873@gmail.com>
2019-02-09 15:41:34 -0800

    added stall for cargo
    made it so the cargointake auto stops when it get's a ball

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -15,7 +15,10 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 
 
-//Declarations
+// Declarations
+
+// PDP
+frc::PowerDistributionPanel pdp{0};
 
 // Right Side Motors
 WPI_TalonSRX BackRightBack{15};
@@ -40,6 +43,7 @@ frc::ADXRS450_Gyro Gyro{};
 
 //Cargo Intake
 VictorSPX FrontLeftMid{4};
+int Spiked = 0;
 
 // Pneumatics/ Lift
 frc::Solenoid CargoIntake{0};
@@ -57,7 +61,16 @@ frc::SpeedControllerGroup Elevator{FrontRightBack, FrontRightMid};
 frc::Joystick JoyAccel1{0}, Xbox{1}, RaceWheel{2};
 
 // LimeLight
+
 std::shared_ptr<NetworkTable> LimeTable = NetworkTable::GetTable("limelight");
+
+//Variables for intake
+bool threeFirstPressed = false;
+float intakeCurrentStart, intakeCurrentEnd;
+int intakeCurrentCounter = 0;
+int intakeCurrentFrames = 3;
+int intakeCurrentThreshold = 10;
+bool intakeStalled = false;
 
 //Straightens out the bot
 float LastSumAngle;
@@ -97,7 +110,9 @@ void Robot::AutonomousPeriodic() {
 }
 
 /*Called when teleop is enabled*/
-void Robot::TeleopInit() {}
+void Robot::TeleopInit() {
+
+}
 
 /*Called every robot packet in teleop*/
 void Robot::TeleopPeriodic() {
@@ -123,15 +138,75 @@ void Robot::TeleopPeriodic() {
     HatchButton = false;
   }
 
-  // Intakes the ball
-  if (Xbox.GetRawButton(3)){
-    FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -.5);
-    std::cout << FrontLeftMid.GetBusVoltage() << std::endl;
-  // Spits the ball
-  } else if (Xbox.GetRawButton(1)){
-    FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 1);
-  } else {
-    FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+  // Intakes the ball when button 3 is pressed
+  if (Xbox.GetRawButton(3))
+  {
+    
+    //Check if the intakeStalled variable is false, meaning that the intake motor is not currently stalling
+    if (!intakeStalled)
+    {
+
+      //If the motor is not currently stalled, a counter intakeCurrentCounter is started. This counter is 3 frames long. 
+      if (intakeCurrentCounter == 0) {
+      
+        //At the start of the counter, store the current electrical current into a variable named intakeCurrentStart
+        //Also raise the counter by 1 so that this section of code only runs once and the counter is initialized.
+        intakeCurrentStart = pdp.GetCurrent(4);
+        intakeCurrentCounter = intakeCurrentCounter + 1;
+
+      }
+      //This next line artifically creates a delay of 3 frames, increasing the counter value by 1 with every passing frame
+      else if (intakeCurrentCounter < intakeCurrentFrames) intakeCurrentCounter = intakeCurrentCounter + 1;
+      //Once the counter reaches 3, three frames have passed and its now time to check the current electrical current again
+      else
+      {
+
+        //After the 3 frame delay, we check for the electrical current again and store this second value in a variable named
+        //intakeCurrentEnd
+        //We also reset the counter back to 0 so that this continues to work as long as button 3 is being pressed
+        intakeCurrentEnd = pdp.GetCurrent(4);
+        intakeCurrentCounter = 0;
+        
+        //Now we check for the difference between the electrical current at the start of the 3 frames and at the end of the 3 frames
+        //If the difference between the two is very low, in this case less than 2, then we know that the intake is either stalling
+        //or that the intake is currently not intaking a ball at all.
+        //The next argument in the if statement checks if the electrical current at the end of the 3 frames is greater than the
+        //threshold, currently set to 10.
+        //This argument lets us know that the motor is probably stalling, to differentiate it from just not intaking anything
+        if ((abs(intakeCurrentEnd - intakeCurrentStart) < 2) && intakeCurrentEnd > intakeCurrentThreshold)
+        {
+
+          //If both of the above arguments are true, we set the intake motor to zero because it must be stalling
+          //We also set intakeStalled variable to true so that the whole system does not start over until button 3 is released
+          //and pressed again
+          FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+          intakeStalled = true;
+
+        }
+        //If either one of the above arguements are false, it must not be stalling because of an intaked ball so it continues to spin
+        //the motor
+        else FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.5);
+
+      }
+
+    }
+
+  }
+  //When button 3 is not pressed, we set the intake motor power to zero.
+  //Since the motor cannot be stalling if it's not even running, we also set the intakeStalled variable to false
+  else
+  { 
+  
+    //Spit the ball if button 1 is pressed when button 3 is not being pressed
+    if (Xbox.GetRawButton(1)) FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.5);
+    else 
+    {
+      
+      FrontLeftMid.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+      intakeStalled = false;
+
+    }
+
   }
 
   if (RaceWheel.GetRawButton(5)){
